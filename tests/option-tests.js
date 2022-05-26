dde35dedb6df5f9fc61c116c2af1e1a5c89488f2
lvermett <1619661+latonv@users.noreply.github.com>
2022-05-26 16:30:38 -0700

    Added tests for the option to specify which bases to test against

--- tests/option-tests.js
+++ tests/option-tests.js
@@ -5,7 +5,7 @@ chai.use(chaiAsPromised).should();
 const { testPrimality } = require("../index.js");
 
 describe("input options", () => {
-  it("should correctly mark primes with user-specified numRounds", async () => {
+  it("should correctly label primes with user-specified numRounds", async () => {
     const primes = [
       "89",
       "4145835283301077"
@@ -15,7 +15,7 @@ describe("input options", () => {
     }
   });
 
-  it("should correctly mark composite numbers with user-specified numRounds", async () => {
+  it("should correctly label composite numbers with user-specified numRounds", async () => {
     const composites = [
       "91",
       "41458352833010723"
@@ -23,6 +23,23 @@ describe("input options", () => {
     for (const n of composites) {
       await testPrimality(n, { numRounds: 8 }).should.eventually.be.an("object").and.have.property("probablePrime", false);
     }
+  });
+
+  it("should correctly use a provided array of bases instead of random bases", async () => {
+    // The first eight primes are all strong liars for this composite number...
+    await testPrimality(341550071728321n, { bases: [2, 3, 5, 7, 11, 13, 17, 19] })
+      .should.eventually.be.an("object").and.have.property("probablePrime", true); 
+
+    // ...but 23 is not!
+    await testPrimality(341550071728321n, { bases: [23] })
+      .should.eventually.be.an("object").and.have.property("probablePrime", false); 
+  });
+
+  it("should ignore the numRounds option when bases is specified", async () => {
+    // If it only tests this number with base 2, it should incorrectly label it a probable prime
+    // But if it runs 20 tests with random bases, it will almost certainly be found composite
+    await testPrimality(341550071728321n, { numRounds: 20, bases: [2] })
+      .should.eventually.be.an("object").and.have.property("probablePrime", true);
   });
 
   it("should not return a divisor if findDivisor=false", async () => {
